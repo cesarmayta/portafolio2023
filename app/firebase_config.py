665026a0e7ffe105d9ee23aa222a69e51ff79e0a
César Mayta <cesarmayta@gmail.com>
2023-05-27 11:46:02 -0500

    deploy 6 sin tokens

--- app/firebase_config.py
+++
@@ -1,9 +0,0 @@
-firebaseConfig = {
-  "apiKey": "AIzaSyBp4jQjiLim5ciTHAVMGbsxlw7yzJ4Zwus",
-  "authDomain": "cmaytacodigog20-portafolio.firebaseapp.com",
-  "projectId": "cmaytacodigog20-portafolio",
-  "storageBucket": "cmaytacodigog20-portafolio.appspot.com",
-  "messagingSenderId": "479793364143",
-  "appId": "1:479793364143:web:3ded98017563344bc4685a",
-  "databaseURL":""
-}
